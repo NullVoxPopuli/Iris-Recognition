fd572e6c2848d6063763707272bc8ad1d7c98b92
MiliasColds <thorshammer01889@gmail.com>
2011-02-14 16:52:25 -0500

    images of comparisons

--- compare_imgs.m
+++ compare_imgs.m
@@ -1,9 +1,14 @@
 %Compareimgs
 
-img1 = imread('image6.png');
-img2 = imread('image6.png');
+img1 = imread('image9.png');
+img2 = imread('image10.png');
 [im_1 FV1] = main(img1);
 [im_2 FV2] = main(img2);
 
 D = getDistanceCo(FV1,FV2);
 disp(D);
+
+imwrite(uint8(im_1),'image_e_iris.png');
+imwrite(uint8(im_2),'image_f_iris.png');
+imwrite(reshape(FV1 / max(FV1), 255, 96),'image_e_FV.png');
+imwrite(reshape(FV2 / max(FV2), 255, 96),'image_f_FV.png');
